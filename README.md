6deb0cf360ad34692c9bf4f50532ba28f4bdb331
zeng.hongpeng <zeng.hongpeng_wb2017@hellogroup.com>
2023-11-08 12:41:23 +0800

    feat:

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Electron Play App
+
+<!-- 
+1、主进程编译，重启Window
+2、preload, renderer 编译，刷新页面
+ -->
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
-const { app, BrowserWindow } = require('electron');
-const path = require('path');
+import path from 'node:path'
+import {app, BrowserWindow} from 'electron'
 
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) {
