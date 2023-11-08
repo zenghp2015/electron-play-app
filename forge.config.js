1cf605c5a9a75a88f6a27ca4a1407e4b89dc3dd9
zeng.hongpeng <zeng.hongpeng_wb2017@hellogroup.com>
2023-11-08 12:27:52 +0800

    feat: vite plugin

--- forge.config.js
+++ forge.config.js
@@ -23,10 +23,36 @@ module.exports = {
     },
   ],
   plugins: [
+    // {
+    //   // https://www.electronforge.io/config/plugins/auto-unpack-natives
+    //   name: '@electron-forge/plugin-auto-unpack-natives',
+    //   config: {},
+    // },
     {
-      // https://www.electronforge.io/config/plugins/auto-unpack-natives
-      name: '@electron-forge/plugin-auto-unpack-natives',
-      config: {},
-    },
+      // vite
+      name: '@electron-forge/plugin-vite',
+      config: {
+        // `build` can specify multiple entry builds, which can be
+        // Main process, Preload scripts, Worker process, etc.
+        build: [
+          {
+            // `entry` is an alias for `build.lib.entry`
+            // in the corresponding file of `config`.
+            entry: 'src/main.js',
+            config: 'vite.main.config.mjs'
+          },
+          {
+            entry: 'src/preload.js',
+            config: 'vite.preload.config.mjs'
+          }
+        ],
+        renderer: [
+          {
+            name: 'main_window',
+            config: 'vite.renderer.config.mjs'
+          }
+        ]
+      }
+    }
   ],
 };
